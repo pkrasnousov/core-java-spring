190ebf3d746c75d1499009dda9c1237751de1db0
Pablo Puñal Pereira <pablo.punal@thingwave.eu>
2020-06-08 10:49:20 +0200

    mqtt schema updated

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/protocols/mqtt/MqttServiceRegistry.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/protocols/mqtt/MqttServiceRegistry.java
@@ -69,7 +69,6 @@ public class MqttServiceRegistry {
     @Value(CommonConstants.$TRUSTSTORE_PASSWORD)
     private String trustStorePassword;
 
-
     //=================================================================================================
     // methods
     //-------------------------------------------------------------------------------------------------
@@ -91,5 +90,317 @@ public class MqttServiceRegistry {
         // Start MQTT here
         logger.info("Starting MQTT");
     }
+    
+    /* /echo - GET 
+    exchange.respond(
+                    ResponseCode.CONTENT,
+                    "Got it!",
+                    MediaTypeRegistry.TEXT_PLAIN);
+    */
 
+    /* /services - GET 
+    
+        try {
+                Map<String, String> queries = CoapTools.getQueryParams(exchange);
+
+                if ((!queries.containsKey(PAGE_KEY) && queries.containsKey(SIZE_KEY))
+                        || (queries.containsKey(PAGE_KEY) && !queries.containsKey(SIZE_KEY))) {
+                    throw new Exception("Not valid Parameters");
+                }
+
+                int validatedPage = CoapTools.getParam(queries, PAGE_KEY, PAGE_DEFAULT);
+                int validatedSize = CoapTools.getParam(queries, SIZE_KEY, SIZE_DEFAULT);
+                String direction = CoapTools.getParam(queries, DIRECTION_KEY, DIRECTION_DEFAULT);
+                String sortField = CoapTools.getParam(queries, SIZE_KEY, SORT_DEFAULT);
+                Direction validatedDirection = CoreUtilities.calculateDirection(direction, URL_PATH_MGMT + "/" + URL_PATH_SERVICES);
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.getServiceDefinitionEntriesResponse(validatedPage, validatedSize, validatedDirection, sortField)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    
+     */
+ /* /services - POST 
+    try {
+                ServiceDefinitionRequestDTO serviceDefinitionRequestDTO = mapper.readValue(exchange.getRequestText(), ServiceDefinitionRequestDTO.class);
+
+                if (Utilities.isEmpty(serviceDefinitionRequestDTO.getServiceDefinition())) {
+                    throw new Exception("Service definition is null or blank");
+                }
+
+                for (CoreSystemService coreSystemService : CoreSystemService.values()) {
+                    if (coreSystemService.getServiceDefinition().equalsIgnoreCase(serviceDefinitionRequestDTO.getServiceDefinition().trim())) {
+                        throw new Exception("serviceDefinition '" + serviceDefinitionRequestDTO.getServiceDefinition() + "' is a reserved arrowhead core system service.");
+                    }
+                }
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsBytes(serviceRegistryDBService.createServiceDefinitionResponse(serviceDefinitionRequestDTO.getServiceDefinition())),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+     */
+    
+    /* /services/{id} - GET 
+    try {
+                int serviceId = Integer.parseInt(CoapTools.getUrlPathValue(exchange, URL_PATH_ID));
+
+                if (serviceId < 1) {
+                    throw new Exception(String.format("Id %d not valid!", serviceId));
+                }
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.getServiceDefinitionByIdResponse(serviceId)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /services/{id} - PUT 
+    try {
+                int serviceId = Integer.parseInt(CoapTools.getUrlPathValue(exchange, URL_PATH_ID));
+
+                if (serviceId < 1) {
+                    throw new Exception(String.format("Id %d not valid!", serviceId));
+                }
+
+                ServiceDefinitionRequestDTO serviceDefinitionRequestDTO = mapper.readValue(exchange.getRequestText(), ServiceDefinitionRequestDTO.class);
+
+                if (Utilities.isEmpty(serviceDefinitionRequestDTO.getServiceDefinition())) {
+                    throw new Exception("Service definition is null or blank");
+                }
+
+                for (CoreSystemService coreSystemService : CoreSystemService.values()) {
+                    if (coreSystemService.getServiceDefinition().equalsIgnoreCase(serviceDefinitionRequestDTO.getServiceDefinition().trim())) {
+                        throw new Exception("serviceDefinition '" + serviceDefinitionRequestDTO.getServiceDefinition() + "' is a reserved arrowhead core system service.");
+                    }
+                }
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.updateServiceDefinitionByIdResponse(serviceId, serviceDefinitionRequestDTO.getServiceDefinition())),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /services/{id} - DELETE 
+    try {
+                int serviceId = Integer.parseInt(CoapTools.getUrlPathValue(exchange, URL_PATH_ID));
+
+                if (serviceId < 1) {
+                    throw new Exception(String.format("Id %d not valid!", serviceId));
+                }
+                serviceRegistryDBService.removeServiceDefinitionById(serviceId);
+                exchange.respond(ResponseCode.VALID);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /register - POST 
+    try {
+
+                ServiceRegistryRequestDTO serviceRegistryRequestDTO = mapper.readValue(exchange.getRequestText(), ServiceRegistryRequestDTO.class);
+                String origin = URL_PATH_MGMT + "/" + URL_PATH_REGISTER;
+
+                if (Utilities.isEmpty(serviceRegistryRequestDTO.getServiceDefinition())) {
+                    throw new Exception("Service definition is null or blank");
+                }
+
+                if (!Utilities.isEmpty(serviceRegistryRequestDTO.getEndOfValidity())) {
+                    try {
+                        Utilities.parseUTCStringToLocalZonedDateTime(serviceRegistryRequestDTO.getEndOfValidity().trim());
+                    } catch (final DateTimeParseException ex) {
+                        throw new Exception("End of validity is specified in the wrong format. Please provide UTC time using " + Utilities.getDatetimePattern() + " pattern.");
+                    }
+                }
+
+                ServiceSecurityType securityType = null;
+                if (serviceRegistryRequestDTO.getSecure() != null) {
+                    for (final ServiceSecurityType type : ServiceSecurityType.values()) {
+                        if (type.name().equalsIgnoreCase(serviceRegistryRequestDTO.getSecure())) {
+                            securityType = type;
+                            break;
+                        }
+                    }
+
+                    if (securityType == null) {
+                        throw new Exception("Security type is not valid.");
+                    }
+                } else {
+                    securityType = ServiceSecurityType.NOT_SECURE;
+                }
+
+                if (securityType != ServiceSecurityType.NOT_SECURE && serviceRegistryRequestDTO.getProviderSystem().getAuthenticationInfo() == null) {
+                    throw new Exception("Security type is in conflict with the availability of the authentication info.");
+                }
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.registerServiceResponse(serviceRegistryRequestDTO)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /unregister - DELETE 
+    try {
+                Map<String, String> queries = CoapTools.getQueryParams(exchange);
+
+                String serviceDefinition = CoapTools.getParam(queries, UNREGISTER_SERVICE_DEFINITION_KEY, NULL_DEFAULT);
+                String providerName = CoapTools.getParam(queries, UNREGISTER_SERVICE_PROVIDER_SYSTEM_NAME_KEY, NULL_DEFAULT);
+                String providerAddress = CoapTools.getParam(queries, UNREGISTER_SERVICE_PROVIDER_ADDRESS_KEY, NULL_DEFAULT);
+                int providerPort = CoapTools.getParam(queries, UNREGISTER_SERVICE_PROVIDER_PORT_KEY, 0);
+
+                if (Utilities.isEmpty(serviceDefinition)) {
+                    throw new Exception("Service definition is blank");
+                }
+
+                if (Utilities.isEmpty(providerName)) {
+                    throw new Exception("Name of the provider system is blank");
+                }
+
+                if (Utilities.isEmpty(providerAddress)) {
+                    throw new Exception("Address of the provider system is blank");
+                }
+
+                if (providerPort < CommonConstants.SYSTEM_PORT_RANGE_MIN || providerPort > CommonConstants.SYSTEM_PORT_RANGE_MAX) {
+                    throw new Exception("Port must be between " + CommonConstants.SYSTEM_PORT_RANGE_MIN + " and " + CommonConstants.SYSTEM_PORT_RANGE_MAX + ".");
+                }
+                serviceRegistryDBService.removeServiceRegistry(serviceDefinition, providerName, providerAddress, providerPort);
+                exchange.respond(ResponseCode.VALID);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /query - POST 
+    try {
+                ServiceQueryFormDTO serviceQueryFormDTO = mapper.readValue(exchange.getRequestText(), ServiceQueryFormDTO.class);
+
+                if (Utilities.isEmpty(serviceQueryFormDTO.getServiceDefinitionRequirement())) {
+                    throw new Exception("Service definition requirement is null or blank");
+                }
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.queryRegistry(serviceQueryFormDTO)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /query/all - GET 
+    try {
+                int page = 0;
+                int size = Integer.MAX_VALUE;
+                Direction direction = Direction.ASC;
+                String sortField = CommonConstants.COMMON_FIELD_NAME_ID;
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.getServiceRegistryEntriesResponse(page, size, direction, sortField)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /query/system - POST 
+    try {
+                SystemRequestDTO systemRequestDTO = mapper.readValue(exchange.getRequestText(), SystemRequestDTO.class);
+
+                if (systemRequestDTO == null) {
+                    throw new Exception("System is null.");
+                }
+
+                if (Utilities.isEmpty(systemRequestDTO.getSystemName())) {
+                    throw new Exception("System Name is null.");
+                }
+
+                if (Utilities.isEmpty(systemRequestDTO.getAddress())) {
+                    throw new Exception("Empty System Address");
+                }
+
+                if (systemRequestDTO.getPort() == null) {
+                    throw new Exception("Empty Port");
+                }
+
+                final int validatedPort = systemRequestDTO.getPort().intValue();
+                if (validatedPort < CommonConstants.SYSTEM_PORT_RANGE_MIN || validatedPort > CommonConstants.SYSTEM_PORT_RANGE_MAX) {
+                    throw new Exception("Port must be between " + CommonConstants.SYSTEM_PORT_RANGE_MIN + " and " + CommonConstants.SYSTEM_PORT_RANGE_MAX + ".");
+                }
+
+                String systemName = systemRequestDTO.getSystemName();
+                String address = systemRequestDTO.getAddress();
+                int port = systemRequestDTO.getPort();
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.getSystemByNameAndAddressAndPortResponse(systemName, address, port)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
+    
+    /* /query/system/{common_field_name_id} - GET 
+    try {
+                int systemId = Integer.parseInt(CoapTools.getUrlPathValue(exchange, URL_PATH_ID));
+
+                if (systemId < 1) {
+                    throw new Exception(String.format("Id %d not valid!", systemId));
+                }
+
+                exchange.respond(
+                        ResponseCode.CONTENT,
+                        mapper.writeValueAsString(serviceRegistryDBService.getSystemById(systemId)),
+                        MediaTypeRegistry.APPLICATION_JSON);
+            } catch (Exception ex) {
+                exchange.respond(
+                        ResponseCode.INTERNAL_SERVER_ERROR,
+                        ex.getMessage(),
+                        MediaTypeRegistry.TEXT_PLAIN);
+            }
+    */
 }
