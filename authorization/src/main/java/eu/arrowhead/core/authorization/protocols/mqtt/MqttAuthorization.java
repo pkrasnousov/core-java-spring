dd80087de546f78f20ad38577f8359926d6a08ee
Jens Eliasson <jens.eliasson@gmail.com>
2020-10-15 08:56:13 +0200

    code cleanup

--- authorization/src/main/java/eu/arrowhead/core/authorization/protocols/mqtt/MqttAuthorization.java
+++ authorization/src/main/java/eu/arrowhead/core/authorization/protocols/mqtt/MqttAuthorization.java
@@ -5,17 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.annotation.PostConstruct;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.CoreCommonConstants;
 import eu.arrowhead.common.Utilities;
-import eu.arrowhead.common.coap.AhCoapServer;
-import eu.arrowhead.common.coap.configuration.CoapCertificates;
-import eu.arrowhead.common.coap.configuration.CoapCredentials;
-import eu.arrowhead.common.coap.configuration.CoapServerConfiguration;
-import eu.arrowhead.common.coap.tools.CoapTools;
 
 import eu.arrowhead.common.dto.internal.AuthorizationInterCloudCheckRequestDTO;
 import eu.arrowhead.common.dto.internal.AuthorizationInterCloudCheckResponseDTO;
@@ -46,11 +42,14 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Resource;
-import org.eclipse.californium.core.CoapResource;
-import org.eclipse.californium.core.coap.CoAP;
-import org.eclipse.californium.core.coap.MediaTypeRegistry;
-import org.eclipse.californium.core.server.resources.CoapExchange;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 @Component
 public class MqttAuthorization {
@@ -214,176 +213,4 @@ public class MqttAuthorization {
         }
     }
     
-    /* /echo - GET 
-    exchange.respond(
-                    CoAP.ResponseCode.CONTENT,
-                    "Got it!",
-                    MediaTypeRegistry.TEXT_PLAIN);
-    */
-    
-    /* /intercloud/check - POST 
-    try {
-                logger.debug("New AuthorizationInterCloud check request recieved");
-                AuthorizationInterCloudCheckRequestDTO request = mapper.readValue(exchange.getRequestText(), AuthorizationInterCloudCheckRequestDTO.class);
-
-                final boolean isCloudInvalid = request.getCloud() == null;
-                final boolean isCloudOperatorInvalid = isCloudInvalid || Utilities.isEmpty(request.getCloud().getOperator());
-                final boolean isCloudNameInvalid = isCloudInvalid || Utilities.isEmpty(request.getCloud().getName());
-                final boolean isServiceDefinitionInvalid = Utilities.isEmpty(request.getServiceDefinition());
-                final boolean isProvidersWithInterfacesListInvalid = request.getProviderIdsWithInterfaceIds() == null || request.getProviderIdsWithInterfaceIds().isEmpty();
-
-                if (isCloudOperatorInvalid || isCloudNameInvalid || isServiceDefinitionInvalid || isProvidersWithInterfacesListInvalid) {
-                    String exceptionMsg = "Payload is invalid due to the following reasons:";
-                    exceptionMsg = isCloudOperatorInvalid ? exceptionMsg + " cloud operator is empty, " : exceptionMsg;
-                    exceptionMsg = isCloudNameInvalid ? exceptionMsg + " cloud name is empty, " : exceptionMsg;
-                    exceptionMsg = isServiceDefinitionInvalid ? exceptionMsg + " serviceDefinition is empty, " : exceptionMsg;
-                    exceptionMsg = isProvidersWithInterfacesListInvalid ? exceptionMsg + " invalid providerIdsWithInterfaceIds list," : exceptionMsg;
-                    exceptionMsg = exceptionMsg.substring(0, exceptionMsg.length() - 1);
-
-                    throw new Exception(exceptionMsg);
-                }
-
-                exchange.respond(
-                        CoAP.ResponseCode.CONTENT,
-                        mapper.writeValueAsString(authorizationDBService.checkAuthorizationInterCloudResponse(request.getCloud().getOperator(), request.getCloud().getName(),
-                                request.getServiceDefinition(), request.getProviderIdsWithInterfaceIds())),
-                        MediaTypeRegistry.APPLICATION_JSON);
-
-            } catch (Exception ex) {
-                exchange.respond(
-                        CoAP.ResponseCode.INTERNAL_SERVER_ERROR,
-                        ex.getMessage(),
-                        MediaTypeRegistry.TEXT_PLAIN);
-            }
-    */
-    
-    /* /intracloud/check - POST 
-    try {
-                logger.debug("New AuthorizationIntraCloud check request recieved");
-                AuthorizationIntraCloudCheckRequestDTO request = mapper.readValue(exchange.getRequestText(), AuthorizationIntraCloudCheckRequestDTO.class);
-
-                final String origin = CommonConstants.AUTHORIZATION_URI + CommonConstants.OP_AUTH_INTRA_CHECK_URI;
-                final SystemRequestDTO consumer = request.getConsumer();
-                if (consumer == null) {
-                    throw new Exception("Consumer is null");
-                }
-
-                checkSystemRequest(consumer, origin, false);
-
-                final boolean isServiceDefinitionIdInvalid = request.getServiceDefinitionId() == null || request.getServiceDefinitionId() < 1;
-                final boolean isProviderListEmpty = request.getProviderIdsWithInterfaceIds() == null || request.getProviderIdsWithInterfaceIds().isEmpty();
-
-                if (isServiceDefinitionIdInvalid || isProviderListEmpty) {
-                    String exceptionMsg = "Payload is invalid due to the following reasons:";
-                    exceptionMsg = isServiceDefinitionIdInvalid ? exceptionMsg + " invalid serviceDefinition id," : exceptionMsg;
-                    exceptionMsg = isProviderListEmpty ? exceptionMsg + " providerId list is empty," : exceptionMsg;
-                    exceptionMsg = exceptionMsg.substring(0, exceptionMsg.length() - 1);
-                    throw new Exception(exceptionMsg);
-                }
-
-                final Set<IdIdListDTO> providerIdsWithInterfaceIdsSet = new HashSet<>();
-
-                final Set<Long> providerIdCheck = new HashSet<>();
-                for (final IdIdListDTO providerWithInterfaces : request.getProviderIdsWithInterfaceIds()) {
-                    if (providerWithInterfaces.getId() != null && providerWithInterfaces.getId() > 0 && !providerIdCheck.contains(providerWithInterfaces.getId())) {
-                        providerIdCheck.add(providerWithInterfaces.getId());
-
-                        final Set<Long> interfaceIdCheck = new HashSet<>();
-                        for (final Long interfaceId : providerWithInterfaces.getIdList()) {
-                            if (interfaceId != null && interfaceId > 0 && !interfaceIdCheck.contains(interfaceId)) {
-                                interfaceIdCheck.add(interfaceId);
-                            } else {
-                                logger.debug("Invalid or duplicated interface id: {} with provider id: {}", interfaceId, providerWithInterfaces.getId());
-                            }
-                        }
-
-                        providerWithInterfaces.getIdList().clear();
-                        providerWithInterfaces.getIdList().addAll(interfaceIdCheck);
-                        providerIdsWithInterfaceIdsSet.add(providerWithInterfaces);
-                    } else {
-                        logger.debug("Invalid or duplicated provider system id: {}", providerWithInterfaces.getId());
-                    }
-                }
-
-                exchange.respond(
-                        CoAP.ResponseCode.CONTENT,
-                        mapper.writeValueAsString(authorizationDBService.checkAuthorizationIntraCloudRequest(consumer.getSystemName(), consumer.getAddress(), consumer.getPort(),
-                                request.getServiceDefinitionId(), providerIdsWithInterfaceIdsSet)),
-                        MediaTypeRegistry.APPLICATION_JSON);
-
-            } catch (Exception ex) {
-                exchange.respond(
-                        CoAP.ResponseCode.INTERNAL_SERVER_ERROR,
-                        ex.getMessage(),
-                        MediaTypeRegistry.TEXT_PLAIN);
-            }
-    */
-    
-    /* /publickey - GET 
-    try {
-                exchange.respond(
-                        CoAP.ResponseCode.CONTENT,
-                        mapper.writeValueAsString(acquireAndConvertPublicKey()),
-                        MediaTypeRegistry.APPLICATION_JSON);
-
-            } catch (Exception ex) {
-                exchange.respond(
-                        CoAP.ResponseCode.INTERNAL_SERVER_ERROR,
-                        ex.getMessage(),
-                        MediaTypeRegistry.TEXT_PLAIN);
-            }
-    */
-    
-    /* /token - POST 
-    try {
-                logger.debug("New token generation request received");
-                TokenGenerationRequestDTO request = mapper.readValue(exchange.getRequestText(), TokenGenerationRequestDTO.class);
-
-                checkTokenGenerationRequest(request);
-
-                exchange.respond(
-                        CoAP.ResponseCode.CONTENT,
-                        mapper.writeValueAsString(tokenGenerationService.generateTokensResponse(request)),
-                        MediaTypeRegistry.APPLICATION_JSON);
-
-            } catch (Exception ex) {
-                exchange.respond(
-                        CoAP.ResponseCode.INTERNAL_SERVER_ERROR,
-                        ex.getMessage(),
-                        MediaTypeRegistry.TEXT_PLAIN);
-            }
-    */
-    
-    /* /subscription/check - POST 
-    try {
-                logger.debug("New AuthorizationEventHandler check request recieved");
-                AuthorizationSubscriptionCheckRequestDTO request = mapper.readValue(exchange.getRequestText(), AuthorizationSubscriptionCheckRequestDTO.class);
-
-                final String origin = CommonConstants.AUTHORIZATION_URI + CommonConstants.OP_AUTH_SUBSCRIPTION_CHECK_URI;
-                final SystemRequestDTO consumer = request.getConsumer();
-                if (consumer == null) {
-                    throw new Exception("Consumer is null");
-                }
-
-                checkSystemRequest(consumer, origin, false);
-
-                if (request.getPublishers() != null && !request.getPublishers().isEmpty()) {
-                    for (final SystemRequestDTO publisher : request.getPublishers()) {
-                        checkSystemRequest(publisher, origin, false);
-                    }
-                }
-
-                exchange.respond(
-                        CoAP.ResponseCode.CONTENT,
-                        mapper.writeValueAsString(authorizationDBService.checkAuthorizationSubscriptionRequest(consumer.getSystemName(), consumer.getAddress(), consumer.getPort(),
-                                request.getPublishers())),
-                        MediaTypeRegistry.APPLICATION_JSON);
-
-            } catch (Exception ex) {
-                exchange.respond(
-                        CoAP.ResponseCode.INTERNAL_SERVER_ERROR,
-                        ex.getMessage(),
-                        MediaTypeRegistry.TEXT_PLAIN);
-            }
-    */
 }
