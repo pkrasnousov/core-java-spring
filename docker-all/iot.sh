f34fbb5d637878cc76ddd0c32d553596ff55b894
Pavel Krasnousov <p.krasnousov@tut.by>
2021-08-17 00:13:44 +0300

    iot.sh

---
+++ docker-all/iot.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod u+x initSQL.sh
+./initSQL.sh
+sudo docker volume create arrowhead_core_mysql
+cd /example
+sudo docker-compose build --build-arg MYSQL_ROOT_PASSWORD=Wasd12321!
